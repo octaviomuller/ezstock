a6675ec6c194b4d47f76ebe5925c183514cf76d1
leoruas <leo.coelho.ruas@gmail.com>
2021-06-10 17:25:34 -0300

    clicking on item now redirects to info screen

--- lib/screens/InfoScreen.dart
+++ lib/screens/InfoScreen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class InfoScreen extends StatelessWidget {
+  //TODO: Change to product
+  final String item;
+  InfoScreen({@required this.item});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/widgets/ListItem.dart
+++ lib/widgets/ListItem.dart
@@ -1,3 +1,4 @@
+import 'package:ezstock/screens/InfoScreen.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
 
@@ -7,47 +8,57 @@ class ListItem extends StatelessWidget {
     Size screenSize = MediaQuery.of(context).size;
     final Random random = new Random();
 
-    return Container(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(8.0),
-            child: Image.asset(
-              'assets/image${random.nextInt(9)}.webp',
-              height: screenSize.height * (0.23),
-              width: screenSize.height * (0.23),
-              fit: BoxFit.cover,
-            ),
-            clipBehavior: Clip.hardEdge,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => InfoScreen(item: 'product'),
           ),
-          Text(
-            "Nome da peça",
-            style: TextStyle(
-              fontWeight: FontWeight.w800,
-              fontSize: screenSize.height * 0.03,
-              color: Colors.grey.shade600,
+        );
+      },
+      child: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Image.asset(
+                'assets/image${random.nextInt(9)}.webp',
+                height: screenSize.height * (0.23),
+                width: screenSize.height * (0.23),
+                fit: BoxFit.cover,
+              ),
+              clipBehavior: Clip.hardEdge,
             ),
-            overflow: TextOverflow.ellipsis,
-          ),
-          Text(
-            "Em estoque: 10",
-            style: TextStyle(
-              fontWeight: FontWeight.w400,
-              fontSize: screenSize.height * 0.02,
-              color: Colors.grey.shade700,
+            Text(
+              "Nome da peça",
+              style: TextStyle(
+                fontWeight: FontWeight.w800,
+                fontSize: screenSize.height * 0.03,
+                color: Colors.grey.shade600,
+              ),
+              overflow: TextOverflow.ellipsis,
             ),
-            overflow: TextOverflow.ellipsis,
-          ),
-          Text(
-            "R\$89,99",
-            style: TextStyle(
-              fontWeight: FontWeight.w600,
-              fontSize: screenSize.height * 0.028,
+            Text(
+              "Em estoque: 10",
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: screenSize.height * 0.02,
+                color: Colors.grey.shade700,
+              ),
+              overflow: TextOverflow.ellipsis,
             ),
-            overflow: TextOverflow.ellipsis,
-          ),
-        ],
+            Text(
+              "R\$89,99",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: screenSize.height * 0.028,
+              ),
+              overflow: TextOverflow.ellipsis,
+            ),
+          ],
+        ),
       ),
     );
   }
